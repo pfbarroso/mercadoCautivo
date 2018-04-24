beaf86f08c5f707c2170a98e54b48bddd7c572ee
Your Name <you@example.com>
2018-04-24 11:25:15 -0300

    Adaptacion

--- lib.h
+++ lib.h
@@ -1,11 +1,10 @@
-
+#include "producto.h"
+#include "usuarios.h"
 typedef struct
 {
     int idUsuario;
     int idProducto;
 
 }eRelaciones;
-
-
 
 int displayMenuABM (char texto[]);
--- main.c
+++ main.c
@@ -1,58 +1,58 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "lib.h"
-#include "usuarios.h"
-#include "producto.h"
 #define CantUsuario 10
 #define CantProduct 100
 
 
 int main()
 {
-    int opcion=-1;
+    int opcion;
     eUsuario users[CantUsuario];
     eUsuario oneUser;
     eRelaciones relaciones[CantProduct*CantUsuario];
 
+/*---------------------------------------------------*/
 
-    indiceUsuarios(users, CantUsuario);
-
-    while (opcion !=0)
-         {
-        opcion=displayMenuABM("\n--- Menu General---\n\n"
+    indiceUsuarios(users, CantUsuario); //Inicializar a cero
+    do
+    {
+        opcion=displayMenuABM(
+                              "\n--- Menu General---\n\n"
                               " 1-  Menu de Usuario "
                               "\n 2-  Menu de Productos"
                               "\n 3-  Comprar producto"
                               "\n 4-  Listar Publicaciones de Usuario "
                               "\n 5-  Listar Publicaciones "
                               "\n 6- Listar usuarios\n \n 0-  SALIR!"
-                              "\t \t Ingrese la opcion deseaba:\t");
-
-            switch (opcion)
-                {
-                    case 1:
-                        menu
-                        break;
-                    case 2:
-                        break;
-                    case 3:
-                        break;
-                    case 4:
-                        break;
-                    case 5:
-                        break;
-                    case 6:
-                        break;
-                    case 0:
-                        opcion=0;
-                        break;
-                    default:
-                        printf("no ha ingresado una opcion valida");
-                        printf("\n");
-                        system ("cls");
-                        break;
-                }
-         }
+                              "\t \t Ingrese la opcion deseaba:\t"
+                              );
+        switch (opcion)
+        {
+        case 1:
+            //menu
+            break;
+        case 2:
+            break;
+        case 3:
+            break;
+        case 4:
+            break;
+        case 5:
+            break;
+        case 6:
+            break;
+        case 0:
+            opcion=0;
+            break;
+        default:
+            printf("no ha ingresado una opcion valida");
+            printf("\n");
+            system ("cls");
+            break;
+        }
+    }
+    while (opcion !=0);
     printf("\n CHAU\n");
     return 0;
 }
